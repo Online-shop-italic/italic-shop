e45add2f39beb1bffb95de55da606b23a80426d1
mtkk077 <154675808+mtkk077@users.noreply.github.com>
2024-04-25 19:02:01 +0600

    added pages and header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
+import React from 'react';
+
 import './App.css';
+import Header from './components/Header/Header.jsx';
+import { Route,Routes} from 'react-router-dom'
+import Women from './pages/Women/Women.jsx'
+import Men from './pages/Men/Men.jsx'
+import Home from './pages/Home/Home.jsx'
+import Jewelry from './pages/Jewelry/Jewelry.jsx'
+import Featured from './pages/Featured/Featured.jsx'
+import SignIn from './pages/SignIn/Sign.jsx';
+
 
 function App() {
   return (
     <div className="App">
-    <p>Start Project</p>
+
+    <Header/>
+
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/women' element={<Women  />} />
+        <Route path='/men' element={<Men />} />
+        <Route path='/jewelry' element={<Jewelry />} />
+        <Route path='/featured' element={<Featured />} />
+        <Route path='/sign-in' element={<SignIn />} />
+    </Routes>
     </div>
   );
 }
